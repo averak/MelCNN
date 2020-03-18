d3b170cddde73233412c7a6c735dcf5976015c9c
AjxLab <abe12@mccc.jp>
2020-03-18 14:59:55 +0900

    データセットを生成

--- build.py
+++ build.py
@@ -24,23 +24,56 @@ def itransform(y, mu=256):
     return x.astype(np.float32)
 
 
-if __name__ == '__main__':
-    config = yaml.load(open('config/wave.yml'))
-    print(config)
+def build_wave(files, config):
+    ## -----*----- 固定長ベクトルに変換 -----*----- ##
+    ret = []
 
-    target_path = config['path']['target']
-    others_path = config['path']['others']
-
-    for file in tqdm(glob(target_path+'*.wav')):
+    for file in tqdm(files):
+        # 音声読み込み
         wav, _ = librosa.load(file, sr=config['wave']['fs'])
         # 無音区間（20dB以下）を除去
         wav, _ = librosa.effects.trim(wav, top_db=20)
         # 最小値０，最大値１に正規化
-        wav = sklearn.preprocessing.minmax_scale(wav)
+        #wav = sklearn.preprocessing.minmax_scale(wav)
         # μ-law変換
         wav = transform(wav)
         # ゼロパディング
         n = 8000 - wav.shape[0] % config['wave']['fs']
         wav = np.append(wav, np.zeros(n))
+        # 固定長に分割
+        wav = np.split(wav, wav.shape[0] / config['wave']['fs'])
 
-        print(wav)
+        for w in wav:  ret.append(w)
+
+    return ret
+
+
+
+if __name__ == '__main__':
+    config = yaml.load(open('config/wave.yml'))
+
+    # ファイル一覧を取得
+    target_files = glob(config['path']['target'] + '*')
+    others_files = glob(config['path']['others'] + '*')
+
+    # ファイルを全て固定長のベクトルに変換
+    target_waves = build_wave(target_files, config)
+    others_waves = build_wave(others_files, config)
+
+    # データ個数（固定長ベクトル）
+    n_data = max([len(target_waves), len(others_waves)])
+
+    # 入力，正解ラベル
+    x, y = [], []
+
+    for i in range(n_data):
+        w1 = target_waves[i % len(target_waves)]
+        w2 = others_waves[i % len(others_waves)]
+        # 合成して正規化
+        mixed = sklearn.preprocessing.minmax_scale(w1 + w2)
+
+        x.append(mixed)
+        y.append(w1)
+
+    x = np.array(x)
+    y = np.array(y, dtype=np.int8)
