90e61d4d689e24a16aaade9b64d313c1281ce585
AjxLab <abe12@mccc.jp>
2020-03-18 14:29:28 +0900

    データの前処理

--- build.py
+++ build.py
@@ -35,11 +35,12 @@ if __name__ == '__main__':
         wav, _ = librosa.load(file, sr=config['wave']['fs'])
         # 無音区間（20dB以下）を除去
         wav, _ = librosa.effects.trim(wav, top_db=20)
-
+        # 最小値０，最大値１に正規化
+        wav = sklearn.preprocessing.minmax_scale(wav)
+        # μ-law変換
+        wav = transform(wav)
         # ゼロパディング
         n = 8000 - wav.shape[0] % config['wave']['fs']
         wav = np.append(wav, np.zeros(n))
-        # 最小値０，最大値１に正規化
-        #wav = sklearn.preprocessing.minmax_scale(wav)
 
-        print(transform(wav).shape)
+        print(wav)
