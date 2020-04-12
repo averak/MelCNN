988f0c016a3c894815e2dc8170211f2cb9b4221a
AjxLab <abe12@mccc.jp>
2020-04-13 04:14:04 +0900

    separate

--- separate.py
+++ separate.py
@@ -7,6 +7,7 @@ import librosa
 from scipy import signal
 import scipy.io.wavfile as wf
 import librosa
+from tqdm import tqdm
 from melcnn import *
 from train import *
 
@@ -17,11 +18,18 @@ SIZE = int(CONFIG['wave']['fs'] * CONFIG['wave']['sec'])
 target_files = glob(CONFIG['path']['target'] + '*')
 others_files = glob(CONFIG['path']['others'] + '*')
 # ファイルを全て固定長のベクトルに変換
-target_waves = build_wave(target_files)
+target_waves = build_wave([target_files[0]])
+others_waves = build_wave(others_files)
 
 melcnn = MelCNN()
 melcnn.load_model()
-w = melcnn.vocoder(target_waves[0])
-print(w)
-wf.write('data/tmp/分離後.wav', CONFIG['wave']['fs'], w*100)
-wf.write('data/tmp/分離前.wav', CONFIG['wave']['fs'], np.array(target_waves[0], dtype=np.int16)*100)
+
+noises = np.array(others_waves[:len(target_waves)])
+wav = []
+for i in tqdm(range(len(target_waves))):
+    target_waves[i] += noises[i]
+    wav.append(melcnn.vocoder(target_waves[i]))
+wav = np.array(wav).flatten()
+
+wf.write('data/tmp/分離後.wav', CONFIG['wave']['fs'], wav)
+wf.write('data/tmp/分離前.wav', CONFIG['wave']['fs'], np.array(target_waves, dtype=np.int16).flatten())
